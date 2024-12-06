5ffc5ac907572ad926a3c561dd30a9738cb7cb84
NetalSarda <netalsarda11@gmail.com>
2024-12-07 00:38:04 +0530

    worked on google authentication also modified custom buttons

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,18 +1,29 @@
-import firebase, { initializeApp } from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
-import { config } from 'webpack';
+// Import only the functions you need from Firebase packages
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
-const Config = 
-    {
-        apiKey: "AIzaSyDiTYCK6xwmgFsS-io1fwL7UScoAOHS318",
-        authDomain: "wearology-fd4e7.firebaseapp.com",
-        projectId: "wearology-fd4e7",
-        storageBucket: "wearology-fd4e7.firebasestorage.app",
-        messagingSenderId: "151896277698",
-        appId: "1:151896277698:web:5a6d615f97c4fbd0f1f15b",
-        measurementId: "G-E3XFWS08EZ"
-      
+// Your Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyDiTYCK6xwmgFsS-io1fwL7UScoAOHS318",
+  authDomain: "wearology-fd4e7.firebaseapp.com",
+  projectId: "wearology-fd4e7",
+  storageBucket: "wearology-fd4e7.appspot.com",
+  messagingSenderId: "151896277698",
+  appId: "1:151896277698:web:5a6d615f97c4fbd0f1f15b",
+  measurementId: "G-E3XFWS08EZ"
 };
-firebase.initializeApp(config);
 
+// Initialize Firebase app
+const firebaseApp = initializeApp(firebaseConfig);
+
+// Get authentication and Firestore services
+export const auth = getAuth(firebaseApp);
+export const firestore = getFirestore(firebaseApp);
+
+// Set up Google authentication provider
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+
+// Google sign-in function
+export const signInWithGoogle = () => signInWithPopup(auth, provider);
