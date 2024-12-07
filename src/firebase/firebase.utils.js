1fc5adcf7ca2e580e7676a41b522d9dacda655f0
Netal Sarda <149175948+NetalSarda@users.noreply.github.com>
2024-12-07 13:15:13 +0530

    Update firebase.utils.js

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,16 +3,6 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-// Your Firebase config
-const firebaseConfig = {
-  apiKey: "AIzaSyDiTYCK6xwmgFsS-io1fwL7UScoAOHS318",
-  authDomain: "wearology-fd4e7.firebaseapp.com",
-  projectId: "wearology-fd4e7",
-  storageBucket: "wearology-fd4e7.appspot.com",
-  messagingSenderId: "151896277698",
-  appId: "1:151896277698:web:5a6d615f97c4fbd0f1f15b",
-  measurementId: "G-E3XFWS08EZ"
-};
 
 // Initialize Firebase app
 const firebaseApp = initializeApp(firebaseConfig);
